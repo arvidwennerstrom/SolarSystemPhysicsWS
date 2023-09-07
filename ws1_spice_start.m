e368817d844133b85ca0fa0a85f46f5c83c662ec
arvidwennerstrom <133912921+arvidwennerstrom@users.noreply.github.com>
2023-09-07 15:45:12 +0200

    All planets

--- ws1_spice_start.m
+++ ws1_spice_start.m
@@ -1,3 +1,4 @@
+
 %% ------------------------------------------------------------------------
 %
 % Lorenz Roth / 1 Sep 2022 
@@ -7,8 +8,8 @@
 
 % Your directory where SPICE toolkit and kernels are located
 % ! Change to the correct directory on your computer ! 
-% dir_spice = '/Users/arvwe/OneDrive/Dokument/MATLAB/MATLAB_SolarSystem/';
-dir_spice = 'C:/Users/tomgi/Documents/DD KTH/AA Cours/Solar System/WS1';
+dir_spice = '/Users/arvwe/OneDrive/Dokument/MATLAB/MATLAB_SolarSystem/';
+% dir_spice = 'C:/Users/tomgi/Documents/DD KTH/AA Cours/Solar System/WS1';
 % dir_spice = '\Users\dansf\OneDrive\Documents\KTH\Solar System Physics';
 % dir_spice = '/Users/irene/Documents/KTH/year 2/Solar System Physics/';
 
@@ -24,7 +25,12 @@ addpath( fullfile(dir_spice, 'mice/src/mice') )
 % Standard kernels:
 cspice_furnsh( fullfile(dir_spice, 'kernels/LSK/naif0010.tls') ) % Leapseconds - time conversion
 cspice_furnsh( fullfile(dir_spice, 'kernels/SPK/de440s.bsp') )   % Ephemeris - planets
+cspice_furnsh( fullfile(dir_spice, 'kernels/SPK/mar097.bsp') )   % Ephemeris - Mars + moons system
 cspice_furnsh( fullfile(dir_spice, 'kernels/SPK/jup230l.bsp') )  % Ephemeris - Jupiter + moons system
+cspice_furnsh( fullfile(dir_spice, 'kernels/SPK/sat415.bsp') )   % Ephemeris - Saturn + moons system
+cspice_furnsh( fullfile(dir_spice, 'kernels/SPK/ura111.bsp') )   % Ephemeris - Uranus + moons system
+cspice_furnsh( fullfile(dir_spice, 'kernels/SPK/nep095.bsp') )   % Ephemeris - Neptune + moons system
+cspice_furnsh( fullfile(dir_spice, 'kernels/SPK/plu058.bsp') )   % Ephemeris - Pluto
 cspice_furnsh( fullfile(dir_spice, 'kernels/PCK/pck00010.tpc') ) % Orientation, size, shape - planets
 
 % Extra kernels:
@@ -38,32 +44,51 @@ et_mission  = cspice_str2et(['2016-09-01T12:00';'2028-09-01T12:00']); % From sta
 et_step  = 3600 ; % time step of 1 hour in ephemeris (in seconds)
 et_arr   = et_mission(1):et_step:et_mission(2); % Create a time array from start to end
 
-cspice_str2et('2000-01-01T11:58:56')
+cspice_str2et('2000-01-01T11:58:56');
 
-% Calculate the positions of planet Jupiter ('599') for these times 
+% Calculate the positions of planets
 % - uses Ecliptic Coordinate System, with third coord pointing to Ecliptic North
-[jup_spos, ltime] = cspice_spkpos('599', et_arr, 'ECLIPJ2000', 'LT', 'SUN');
-[mars_spos, ltime2] = cspice_spkpos('499', et_arr, 'ECLIPJ2000', 'LT', 'SUN');
+[mar_spos, mar_ltime] = cspice_spkpos('499', et_arr, 'ECLIPJ2000', 'LT', 'SUN');
+[jup_spos, jup_ltime] = cspice_spkpos('599', et_arr, 'ECLIPJ2000', 'LT', 'SUN');
+[sat_spos, sat_ltime] = cspice_spkpos('699', et_arr, 'ECLIPJ2000', 'LT', 'SUN');
+[ura_spos, ura_ltime] = cspice_spkpos('799', et_arr, 'ECLIPJ2000', 'LT', 'SUN');
+[nep_spos, nep_ltime] = cspice_spkpos('899', et_arr, 'ECLIPJ2000', 'LT', 'SUN');
+[plu_spos, plu_ltime] = cspice_spkpos('999', et_arr, 'ECLIPJ2000', 'LT', 'SUN');
+
 
 % Convert coordinates from [km] to [AU]
 AU_km = 1.495979e+8;    % 1 AU in [km]
-jup_spos=jup_spos/AU_km;
+mar_spos = mar_spos/AU_km;
+jup_spos = jup_spos/AU_km;
+sat_spos = sat_spos/AU_km;
+ura_spos = ura_spos/AU_km;
+nep_spos = nep_spos/AU_km;
+plu_spos = plu_spos/AU_km;
 
 % Find perihelion of Jupiter in that period (minimum distance to Sun)
 % ! [distance in km , array index of minimum distance ]
-[jup_perih,i_perih] = min(cspice_vnorm(jup_spos));    
+[jup_perih,i_perih] = min(cspice_vnorm(jup_spos)); 
 date_perih = cspice_et2utc(et_arr(i_perih),'C',0); % Convert ET of perihelion to date string
-disp(['Jupiter´s perihelion distance is ',num2str(jup_perih), ' AU, on ',date_perih(1:11)]);
+% disp(['Jupiter´s perihelion distance is ',num2str(jup_perih), ' AU, on ',date_perih(1:11)]);
 
-% Plot the orbit of Jupiter 
+% Plot the orbits of Solar system
+outerLimit = 35;
 figure(1); 
-plot(jup_spos(1,:),jup_spos(2,:),'b');
-axis([-7 7 -7 7]);  % Set axes ranges
-hold on;
-plot(jup_spos(1,i_perih),jup_spos(2,i_perih),'-ko','MarkerSize',10,'Color','b'); % Plot Jup position at perhelion
 plot(0,0,'-ko','MarkerSize',10,'Color','y',LineWidth=5); % Plot sun in center
+hold on;
+plot(mar_spos(1,:),mar_spos(2,:),'b');
+plot(jup_spos(1,:),jup_spos(2,:),'b');
+plot(sat_spos(1,:),sat_spos(2,:),'b');
+plot(ura_spos(1,:),ura_spos(2,:),'b');
+plot(nep_spos(1,:),nep_spos(2,:),'b');
+plot(plu_spos(1,:),plu_spos(2,:),'b');
+
+% plot(jup_spos(1,i_perih),jup_spos(2,i_perih),'-ko','MarkerSize',10,'Color','b'); % Plot Jup position at perhelion
 hold off;
+axis([-outerLimit outerLimit -outerLimit outerLimit]);  % Set axes ranges
 xlabel('AU')
 ylabel('AU')
 
 %--------------------------------------------------------------------------
+
+
