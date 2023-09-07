053777d4b26e8b0e2fd0202dfa6f16c9339829ac
Ragnheidur Tryggvadóttir <ragnheidurt3@gmail.com>
2023-09-07 14:43:33 +0200

    adding my directory again

--- ws1_spice_start.m
+++ ws1_spice_start.m
@@ -9,6 +9,7 @@
 % ! Change to the correct directory on your computer ! 
 % dir_spice = '/Users/arvwe/OneDrive/Dokument/MATLAB/MATLAB_SolarSystem/';
 dir_spice = 'C:/Users/tomgi/Documents/DD KTH/AA Cours/Solar System/WS1';
+% dir_spice = '\Users\dansf\OneDrive\Documents\KTH\Solar System Physics';
 
 % Add paths to routines of toolkit so that MATLAB finds routines
 % ! more paths need to be linked than mentioend in description !
