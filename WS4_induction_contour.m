f3b690790f27d7e6cae0c1f76bb3d02e29edf37d
arvidwennerstrom <133912921+arvidwennerstrom@users.noreply.github.com>
2023-12-07 15:36:24 +0100

    Add files via upload

--- WS4_induction_contour.m
+++ WS4_induction_contour.m
@@ -2,20 +2,23 @@ clear all; clc; close all;
 global R_call r_layertop ind_period mu0
 
 % set the parameters
- mu0 = 4e-7*pi; % [kgm/s^2A^2]
- R_call = 2410*1e3; %Callisto's radius
+mu0 = 4e-7*pi; % [kgm/s^2A^2]
+R_call = 2410*1e3; % [m] Callisto's radius
+ind_period=10.2*3600;% [s] synodic period for Callisto
+cond_layer = 0.01:0.1:100; % [S/m] range of the conductivivity of the conducting shell
+
+% When calculating ocean
+r_layertop = R_call-140*1e3; % [m] r0, outer radius of conducting sphere 
+r_layerbot=linspace(1900*1e3,r_layertop-1e2,10000); % [m] r1, range for the inner radius of the conducting shell (see Figure 1)
  
+% When calculating core
+% r_layertop = linspace(500*1e3,700*1e3,10); % [m] outer radius of core
+% r_layerbot=zeros(1,10); % [m] r1, range for the inner radius of the conducting shell (see Figure 1) 
 
- cond_layer=[0.01 1:100]; % [S/m] range of the conductivivity of the conducting shell (S/m)
- r_layertop=R_call-150*1e3; % [m] r0, outer radius of the conducting shell (see Figure 1)
- r_layerbot=linspace(1800*1e3,r_layertop-1e3,10); % [m] r1, range for the inner radius of the conducting shell (see Figure 1)
- ind_period=10.2*3600;% [s] synodic period for Callisto
-
-  
- %create a grid to calculate the amplitude and phase in dependence of the
- %conductivity and thickness
+%create a grid to calculate the amplitude and phase in dependence of the 
+%conductivity and thickness
 [cond_layer1,r_layerbot1]= ndgrid(cond_layer,r_layerbot);
-thickness = r_layertop-r_layerbot1;
+thickness1 = (r_layertop-r_layerbot1); % [m]
 
 %calculate the skin depth
 skin=1./sqrt(mu0*pi/ind_period.*cond_layer1);
@@ -28,16 +31,27 @@ d_skin=(r_layertop-r_layerbot1)./skin;
 %% plot the isocontours
 figure(1)
 subplot(1,2,1)
-contour(log10(cond_layer1),log10(thickness),A,9,'ShowText','on')
+contourf(cond_layer1,thickness1./1000,A,(0.1:0.1:0.9),'ShowText','on')
+ax1 = gca;
+set(ax1,'Ydir','reverse')
+set(ax1,'YScale','log')
+set(ax1,'XScale','log')
+grid on; 
+title('Normalized amplitude: Ocean')
 xlabel('Conductivity [S/m]')
-ylabel('Thickness [m]')
-title('Normalized amplitude')
-grid on
+ylabel('Thickness [km]')
 
 subplot(1,2,2)
-contour(log10(cond_layer1),log10(thickness),phase,9,'ShowText','on')
+contourf(cond_layer1,thickness1./1000,phase,(-10:-10:-80),'ShowText','on')
+ax2 = gca;
+set(ax2,'Ydir','reverse')
+set(ax2,'YScale','log')
+set(ax2,'XScale','log')
+grid on; 
+title('Phase shift [deg]: Ocean')
 xlabel('Conductivity [S/m]')
-ylabel('Thickness [m]')
-title('Phase shift [deg]')
-grid on
+ylabel('Thickness [km]')
+
+figure(2)
+contourf(cond_layer1,thickness1./1000,d_skin,'ShowText','on')
 
