544779c3ca5d6a0bf88b09ec58dd25eef86b5e85
arvidwennerstrom <133912921+arvidwennerstrom@users.noreply.github.com>
2023-12-08 16:30:10 +0100

    Add files via upload

--- WS4_induction_contour.m
+++ WS4_induction_contour.m
@@ -1,43 +1,58 @@
 clear all; clc; close all;
-global R_call r_layertop ind_period mu0
+global R_call ind_period mu0
+
+OCEAN_OR_CORE = 'o';
 
 % set the parameters
 mu0 = 4e-7*pi; % [kgm/s^2A^2]
 R_call = 2410*1e3; % [m] Callisto's radius
 ind_period=10.2*3600;% [s] synodic period for Callisto
-cond_layer = 0.01:0.1:100; % [S/m] range of the conductivivity of the conducting shell
+cond_layer = 0.01:0.01:100; % [S/m] Conductivity range for layers
 
-% When calculating ocean
-r_layertop = R_call-140*1e3; % [m] r0, outer radius of conducting sphere 
-r_layerbot=linspace(1900*1e3,r_layertop-1e2,10000); % [m] r1, range for the inner radius of the conducting shell (see Figure 1)
- 
-% When calculating core
-% r_layertop = linspace(500*1e3,700*1e3,10); % [m] outer radius of core
-% r_layerbot=zeros(1,10); % [m] r1, range for the inner radius of the conducting shell (see Figure 1) 
+if OCEAN_OR_CORE == 'o'
+    % When calculating ocean
+    % Crust assumed to be 200 km deep. Ocean between 0 and 1000 km deep
+    r_layertop = R_call-200*1e3; % [m] r0, outer radius of conducting sphere 
+    r_layerbot = linspace(r_layertop-1000*1e3,r_layertop-100,1e3); % [m] r1, range for the inner radius of the conducting shell (see Figure 1)
+
+elseif OCEAN_OR_CORE == 'c'
+    % When calculating core
+    % Starting at bottom = 0 (not exactly due to math), and core thickness
+    % between 1 and 1000 km
+    r_layertop = linspace(600*1e3,1500*1e3,1e3); % [m] outer radius of core
+    r_layerbot = 1e-2; % [m] r1, inner radius of the conducting shell. Should be close to 0, but not equal 
+end
+
+thickness = r_layertop-r_layerbot; % [m]
 
 %create a grid to calculate the amplitude and phase in dependence of the 
 %conductivity and thickness
-[cond_layer1,r_layerbot1]= ndgrid(cond_layer,r_layerbot);
-thickness1 = (r_layertop-r_layerbot1); % [m]
+[cond_layer1,thickness1]= ndgrid(cond_layer,thickness);
 
 %calculate the skin depth
-skin=1./sqrt(mu0*pi/ind_period.*cond_layer1);
-d_skin=(r_layertop-r_layerbot1)./skin;
+skin=1./sqrt(mu0*pi/ind_period.*cond_layer1); % [m]
+d_skin=(r_layertop-r_layerbot)./skin;
 
 %use the function conducting_sphere to calculate the amplitude and phase of
 %the induced field
-[A,phase]=conducting_sphere(cond_layer1,r_layerbot1);
+[A,phase]=conducting_sphere(cond_layer1,r_layerbot,r_layertop);
+
 
 %% plot the isocontours
 figure(1)
 subplot(1,2,1)
-contourf(cond_layer1,thickness1./1000,A,(0.1:0.1:0.9),'ShowText','on')
+if OCEAN_OR_CORE == 'o'
+    contourf(cond_layer1,thickness1./1000,A,(0.1:0.1:0.9),'ShowText','on')
+    title('Normalized amplitude: Ocean')
+elseif OCEAN_OR_CORE == 'c'
+    contourf(cond_layer1,thickness1./1000,A,9,'ShowText','on')
+    title('Normalized amplitude: Core')
+end
 ax1 = gca;
 set(ax1,'Ydir','reverse')
 set(ax1,'YScale','log')
 set(ax1,'XScale','log')
 grid on; 
-title('Normalized amplitude: Ocean')
 xlabel('Conductivity [S/m]')
 ylabel('Thickness [km]')
 
@@ -48,10 +63,22 @@ set(ax2,'Ydir','reverse')
 set(ax2,'YScale','log')
 set(ax2,'XScale','log')
 grid on; 
-title('Phase shift [deg]: Ocean')
 xlabel('Conductivity [S/m]')
 ylabel('Thickness [km]')
 
+if OCEAN_OR_CORE == 'o'
+    title('Phase shift [deg]: Ocean')
+elseif OCEAN_OR_CORE == 'c'
+    title('Phase shift [deg]: Core')
+end
+
 figure(2)
-contourf(cond_layer1,thickness1./1000,d_skin,'ShowText','on')
+semilogx(cond_layer1,skin(:,1)./1000)
+grid on;
+xlabel('Conductivity [S/m]')
+ylabel('Skin depth [km]')
+title('Skin depth')
+
+
+
 
